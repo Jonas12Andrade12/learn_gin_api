9acb6a41e99114cd0b6eb6be0c6ebee0f2c7a358
Jonas12Andrade12 <jonas.andrade@codacy.com>
2022-11-08 18:14:08 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 44.99},
+	{ID: "2", Title: "Blue Train 2", Artist: "John Coltrane 2", Price: 45.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
